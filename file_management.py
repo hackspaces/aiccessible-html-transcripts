12f5175d5ea8f41e030ebff2284d10e967ae06f6
infectedsanin <86676996+hackspaces@users.noreply.github.com>
2024-04-09 14:51:04 -0700

    Deleted folders

--- file_management.py
+++ file_management.py
@@ -1,10 +1,12 @@
 import streamlit as st
 import os 
 
+file_list = []
+
 def list_files_in_folder(folder_path):
     #List file in the folder
     file_list = os.listdir(folder_path)
-    pass
+    return file_list
 
 def file_manager():
     st.subheader("File Management", divider="blue")
